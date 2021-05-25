0b9a1bf30de91ea02faefa3567bb956783c1dd95
Rturell <rodrigoturell@gmail.com>
2021-05-25 16:28:21 -0300

    Design changes and english translate

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var initialTopOffset = $('#hero').offset().top;
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    // if the top of our browser is inside the section2
+    if (scroll + initialTopOffset >= $('#about').offset().top && scroll + initialTopOffset <= $('#about').offset().top + $('#about').outerHeight()) {
+        $('.btn-resume').addClass('btn-resume2'); // add the class
+    } else {
+        $('.btn-resume').removeClass('btn-resume2'); // remove the class
+    }
+});
+
+//trigger the scroll
+$(window).scroll(); //ensure if you're in current position when page is refreshed
